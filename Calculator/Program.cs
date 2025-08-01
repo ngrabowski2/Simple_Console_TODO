877aa0a6177d0d3d1cd873a0ce57531249004c52
Nate Grabowski <grabowski.61@buckeyemail.osu.edu>
2025-08-01 14:44:13 -0400

    Project Upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+string operation = Console.ReadLine().ToLower();
+
+
+if (operation == "a")
+{
+    Console.WriteLine(firstNumber + " + " +  secondNumber + " = " + (firstNumber + secondNumber));
+} else if (operation == "s")
+{
+    Console.WriteLine(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+} else if (operation == "m")
+{
+    Console.WriteLine(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+} else
+{
+    Console.WriteLine("Invalid Option");
+}
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
---
+++ ToDoList/Program.cs
@@ -0,0 +1,130 @@
+﻿string userInput;
+List<string> todos = new List<string>();
+
+Console.WriteLine("Hello!");
+do
+{
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    userInput = Console.ReadLine().ToLower();
+
+    switch (userInput)
+    {
+        case "s":
+            if (todos.Count > 0)
+            {
+                SeeAllTodos(todos);
+            }
+            else
+            {
+                Console.WriteLine("No TODOs have been added yet");
+            }
+            break;
+        case "a":
+            todos = AddTodo(todos);
+            break;
+        case "r":
+            todos = RemoveTodo(todos);
+            break;
+        case "e":
+            break;
+        default:
+            Console.WriteLine("Incorrect input");
+            break;
+    }
+} while (userInput != "e");
+
+void SeeAllTodos(List<string> todos)
+{
+    for (int i = 0; i < todos.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {todos[i]}");
+    }
+}
+
+List<string> AddTodo(List<string> todos)
+{
+    List<string> result = new List<string>(todos);
+    do
+    {
+        Console.WriteLine("Enter the TODO description");
+        string userInput = Console.ReadLine();
+        if (userInput.Length > 0 && !todos.Contains(userInput))
+        {
+            result.Add(userInput);
+            Console.WriteLine("TODO successfully added: " + result[^1]);
+        }
+        else if (userInput.Length == 0)
+        {
+            Console.WriteLine("The description cannot be empty.");
+        }
+        else if (todos.Contains(userInput))
+        {
+            Console.WriteLine("The description must be unique.");
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    } while (userInput.Length == 0 || todos.Contains(userInput));
+    return result;
+}
+
+List<string> RemoveTodo(List<string> todos)
+{
+    if (todos.Count != 0)
+    {
+        List<string> result = new List<string>(todos);
+        string userInput;
+        do
+        {
+            Console.WriteLine("Select the index of the TODO you want to remove");
+            SeeAllTodos(todos);
+            userInput = Console.ReadLine();
+            int indexToRemove;
+            //Input is successfully parsed
+            if (int.TryParse(userInput, out indexToRemove))
+            {
+                //Index is valid
+                if (indexToRemove > 0 && indexToRemove <= todos.Count)
+                {
+                    Console.WriteLine("TODO removed: " + todos[--indexToRemove]);
+                    result.RemoveAt(indexToRemove);
+                    //Index is out of bounds
+                }
+                else
+                {
+                    Console.WriteLine("The given index is not valid");
+                    //Allows for loop to continue
+                    userInput = "";
+                }
+            }
+            else
+            //Index not parsed
+            {
+                //Empty
+                if (userInput == "")
+                {
+                    Console.WriteLine("Selected index cannot be empty.");
+                    //Index not number or error parsing
+                }
+                else
+                {
+                    Console.WriteLine("The given index is not valid");
+                    //Allows for loop to continue
+                    userInput = "";
+                }
+            }
+        } while (userInput.Length == 0);
+        return result;
+    }
+    else
+    {
+        Console.WriteLine("No TODOs have been added yet");
+        return todos;
+    }
+}
